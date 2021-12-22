be25bdb0a205f29899ed29d17f7dad73a16dfcf1
Félix MIELCAREK <94568134+felixmielcarek@users.noreply.github.com>
2021-12-22 15:46:00 +0100

    Add files via upload

---
+++ src/sae.h
@@ -0,0 +1,35 @@
+/**
+ * \file sae.h
+ * \brief Fichier contenant tous les prototypes des fonctions
+ * \author BENBOULERBAH Billel - MIELCAREK Félix - PIMPAUD Oscar
+ * \date 9 novembre 2021
+ * 
+ * Le fichier inclut les différentes librairies nécessaires à l'utilisation des outils présents dans l'application.
+ * Les prototypes sont placées dans l'ordre d'apparition des fonctions dans le fichier 'sae.c'. 
+ */ 
+
+//	Bibliothèques à inclure
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//	Constantes
+
+#define TTP 10  // TTP = Taille Tableaux Physique = taille physique des tableaux des éléments des stocks
+#define TTN 31  // TTN = Taille Tableaux Nom = taille physique des tableaux des noms des éléments du stocks
+
+//	Prototypes de fonction
+
+void global(void);
+void chargStock(int tabStockRefTel[],char tabStockNomTel[][31],int tabStockQteTel[],float tabStockPrixTel[],int tabStockSeuilTel[],int* nombreTel);
+void affichMenu(void);
+void ajoutStock(int tabStockRefTel[],char tabStockNomTel[][TTN],int tabStockQteTel[],float tabStockPrixTel[],int tabStockSeuilTel[],int* nombreTel);
+void etatStock(int tabStockRefTel[],char tabStockNomTel[][31],int tabStockQteTel[],float tabStockPrixTel[],int tabStockSeuilTel[],int* nombreTel);
+void traitementCommande(int tabStockRefTel[],char tabStockNomTel[][31],int tabStockQteTel[],float tabStockPrixTel[],int tabStockSeuilTel[],int* nombreTel,int recapVentesTel[][2],int* nombreTelVentes);
+void recapitulatifVentes(int tabStockRefTel[],char tabStockNomTel[][31],float tabStockPrixTel[],int* nombreTel,int recapVentesTel[][2],int* nombreTelVentes);
+void approvisionnement(char tabStockNomTel[][31],int tabStockQteTel[],int tabStockSeuilTel[],int* nombreTel);
+void sauvStock(int tabStockRefTel[],char tabStockNomTel[][31],int tabStockQteTel[],float tabStockPrixTel[],int tabStockSeuilTel[],int* nombreTel);
+void devisCommande(int tabStockRefTel[],char tabStockNomTel[][TTN],float tabStockPrixTel[],int* nombreTel);
+int rechercheRefProduit(int tabStockRefTel[],int* nombreTel,int choix);
+
---
+++ src/test_sae.c
@@ -0,0 +1,26 @@
+/**
+ * \file test_sae.c
+ * \brief Fichier contenant la fonction main
+ * \author BENBOULERBAH Billel - MIELCAREK Félix - PIMPAUD Oscar
+ * \date 9 novembre 2021
+ * 
+ * Le fichier inclut dès le début le fichier sae.h.
+ */ 
+
+//	Fichiers à inclure
+
+#include "sae.h"
+
+//	Fonction main
+
+/**
+ * \brief Entrée du programme
+ * \return arrêt normal du programme
+ * 
+ * La fonction n'appelle que la fonction globale pour que le code soit plus clair.
+ */
+
+int main(void){
+	global();
+	return 0;
+}
